9462f8539d0a605b8e80a95234af9d525cb0d7f7
Banuswaroop <chillapurambhanu@gmail.com>
2026-09-25 17:44:18 +0530

    Fix production frontend CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -36,9 +36,13 @@ app = FastAPI(
     }
 )
 
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://smart-expense-tracker-frontend-dxtq.onrender.com"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
